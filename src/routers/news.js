b9299cbb4e64cd081b4f5dbec71820e9315dce84
Nam Vu <namvu170596@gmail.com>
2025-09-20 09:59:28 +0700

    fix js

--- src/routers/news.js
+++ src/routers/news.js
@@ -6,5 +6,5 @@ const newsController = require('../app/controllers/NewsController.js');
 // Route chi tiết bài viết
 router.get('/:slug', newsController.show);
 // Route danh sách tin tức
-router.get('/', newsController.index);
+router.get("/", newsController.index);
 module.exports = router;
