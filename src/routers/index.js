0d0facf8da11101ef5cb1c301c6387135dc4c08a
Nam Vu <namvu170596@gmail.com>
2025-09-20 09:56:11 +0700

    test path

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,7 +3,7 @@ const siteRouter = require('./site.js');
 
 function route(app) {
   app.use("/news", newsRouter);
-  app.use('/', siteRouter);
+  app.use("/", siteRouter);
 }
 
 module.exports = route;
