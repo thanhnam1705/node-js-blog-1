773af7e7a84b06087b573ee8d64fefc94dfdec39
Nam Vu <namvu170596@gmail.com>
2025-09-20 10:02:37 +0700

    addf file

--- src/routers/news.js
+++ src/routers/news.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const newsController = require('../app/controllers/NewsController.js');
 
 // Route chi tiết bài viết
-router.get('/:slug', newsController.show);
+router.get("/:slug", newsController.show);
 // Route danh sách tin tức
 router.get("/", newsController.index);
 module.exports = router;
